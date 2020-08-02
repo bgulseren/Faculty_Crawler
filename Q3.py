321228e6a8be8fb88e5160fd8b9352d9c22d2f2a
Burak Gulseren <gulserenburak@gmail.com>
2020-08-01 23:52:57 -0600

    phone and address scraping done.

--- Q3.py
+++ Q3.py
@@ -20,7 +20,6 @@ all_urls=['html/ECE.html', 'html/CPE.html', 'html/CE.html', 'html/MME.html', 'ht
 
 df_professors = pd.DataFrame(columns = ["firstname", "lastname", "title", "faculty", "email", "homepage"])
 
-# Your solution goes here
 for url in all_urls:
     faculty = url
     file = open(url, 'r')
@@ -49,6 +48,7 @@ for url in all_urls:
         profLine = profLine.replace('<p>','')
         profLine = profLine.replace('Dr.','')
         profLine = profLine.replace(u'\xa0', u' ')
+        profLine = profLine.replace(u'â€™', u'\'')
         profLine = profLine.strip()
         prof_info_list = profLine.split(', ')
 
@@ -68,9 +68,13 @@ for url in all_urls:
         title = prof_info_list[1]
         
         ### Extracting homepage ###
-        homepage = row.find(href=re.compile("/contacts"))
+        homepage = row.find(href=re.compile("/schulich"))
         if homepage is not None:
             homepage = homepage['href']
+        else:
+            homepage = row.find(href=re.compile("/research"))
+            if homepage is not None:
+                homepage = homepage['href']
 
         ### Extracting email address ###
         email = row.find(href=re.compile("mailto"))
@@ -78,7 +82,54 @@ for url in all_urls:
             email = email['href'].replace('mailto:','')
 
         # add a row
-        df_professors = df_professors.append({'firstname': firstname,'lastname': lastname,'title': title,'faculty': faculty,'email': email,'homepage':homepage}, ignore_index=True)
+        df_professors = df_professors.append({'firstname': firstname, 'lastname': lastname, 'title': title, 'faculty': faculty, 'email': email, 'homepage': homepage}, ignore_index=True)
 
 
-print(df_professors)
+#print(df_professors)
+
+phone_list = []
+office_list = []
+
+for homepage in df_professors['homepage']:
+    print('Scraping...')
+    phone_number = office_number = np.nan
+
+    # html = requests.get('https://schulich.ucalgary.ca/contacts/steve-liang', verify=True).text
+
+    if homepage is None:
+        phone_list.append(phone_list)
+        office_list.append(office_number)
+        print('no homepage for this prof, skipping')
+        continue
+
+    html = requests.get(homepage, verify=True).text
+    soup = BeautifulSoup(html, 'lxml')
+    # all prof contact information are children of this special div,
+    contact_info = soup.find('div', class_="col-md-8 contact-section")
+
+    if contact_info is None:
+        phone_list.append(phone_list)
+        office_list.append(office_number)
+        print('no contact info found for this prof, skipping')
+        continue
+
+    ### Extracting phone number ###
+    phone_tag = contact_info.find(href=re.compile("tel:"))
+    if phone_tag is not None:
+        phone_number = phone_tag.string
+    phone_list.append(phone_list)
+
+    ### Extracting office number ###
+    office_tag = contact_info.find(href=re.compile("https://www.ucalgary.ca/map/contactlist/"))
+    if office_tag is not None:
+        office_number = office_tag.string
+    office_list.append(office_number)
+
+    print('Phone_number: ' + str(phone_number) + ' Office_number: ' + str(office_number))
+
+# df_professors['office'] = office_list
+# df_professors['phone'] = phone_list
+
+# print(df_professors)
+
+# df_professors.to_csv(r'export_dataframe.csv', index = False, header=True)
